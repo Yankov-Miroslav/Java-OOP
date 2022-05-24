d701e49a180b842fc8536e987d3c8f390dbf6318
Yankov-Miroslav <m.yankovv@gmail.com>
2022-05-24 21:06:57 +0300

    Working with Abstraction - Lab

---
+++ WorkingWithAbstraction - Lab/HotelReservation/Main.java
@@ -0,0 +1,22 @@
+package HotelReservation;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String[] input = in.nextLine().split("\\s+");
+
+        double pricePerDay = Double.parseDouble(input[0]);
+        int numberOfDays = Integer.parseInt(input[1]);
+        String season = input[2];
+        String discountType = input[3];
+
+        PriceCalculator calculator = new PriceCalculator(pricePerDay, numberOfDays,
+                Season.fromString(season), Discount.fromString(discountType));
+
+        System.out.printf("%.2f%n", calculator.calculate());
+
+    }
+}
