70b876e5762690f86d1a8386904729e0c50e3fc9
Yankov-Miroslav <m.yankovv@gmail.com>
2022-05-28 13:57:39 +0300

    Working with Abstraction - Exercises

---
+++ WorkingWithAbstraction - Exercises/cardsWithPower/Main.java
@@ -0,0 +1,15 @@
+package cardsWithPower;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        CardRanks cardRank = CardRanks.valueOf(scanner.nextLine());
+        CardSuits cardSuit = CardSuits.valueOf(scanner.nextLine());
+        int sum = cardRank.getRankPower() + cardSuit.getPower();
+        System.out.printf("Card name: %s of %s; Card power: %d", cardRank, cardSuit, sum);
+
+    }
+}
